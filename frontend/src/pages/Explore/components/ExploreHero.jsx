2fa8145bfee46b06957c939502d7c57e848f08d5
Abraham <abrhamalem48829@gmail.com>
2026-01-10 09:07:29 +0300

    fix: explore page and doc: added screenshots folder

--- frontend/src/pages/Explore/components/ExploreHero.jsx
+++ frontend/src/pages/Explore/components/ExploreHero.jsx
@@ -1,13 +1,13 @@
 import SearchBar from "./SearchBar";
 import { Search, Plus } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const ExploreHero = ({ searchQuery, setSearchQuery, onSearch }) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <div className="text-center relative">
-      <div className="absolute right-0 top-0 hidden md:block">
+      {/* <div className="absolute right-0 top-0 hidden md:block">
         <button
           onClick={() => navigate('/repository/create')}
           className="flex items-center gap-2 bg-(--primary-button) hover:bg-(--primary-button-hover) text-white px-4 py-2 rounded-lg transition-all active:scale-95 shadow-lg"
@@ -15,7 +15,7 @@ const ExploreHero = ({ searchQuery, setSearchQuery, onSearch }) => {
           <Plus size={18} />
           <span>New Repository</span>
         </button>
-      </div>
+      </div> */}
 
       <div className="flex items-center justify-center gap-2 mb-4">
         <Search className="stroke-2 w-9 h-9" />
@@ -31,7 +31,7 @@ const ExploreHero = ({ searchQuery, setSearchQuery, onSearch }) => {
         className="flex justify-center items-center"
       />
 
-      <div className="mt-4 md:hidden">
+      {/* <div className="mt-4 md:hidden">
         <button
           onClick={() => navigate('/repository/create')}
           className="flex items-center gap-2 bg-(--primary-button) hover:bg-(--primary-button-hover) text-white px-4 py-2 rounded-lg transition-all mx-auto shadow-lg"
@@ -39,7 +39,7 @@ const ExploreHero = ({ searchQuery, setSearchQuery, onSearch }) => {
           <Plus size={18} />
           <span>New Repository</span>
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
